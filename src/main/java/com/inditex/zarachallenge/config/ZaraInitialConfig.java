b89f8e5108d70f795ca72da8f6743769fc03348f
alfredo <vdevendetta77@gmail.com>
2023-09-27 14:38:40 +0200

    change

--- src/main/java/com/inditex/zarachallenge/config/ZaraInitialConfig.java
+++ src/main/java/com/inditex/zarachallenge/config/ZaraInitialConfig.java
@@ -101,5 +101,4 @@ public class ZaraInitialConfig implements CommandLineRunner {
 						LocalDateTime.parse(stock.get(2), DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX"))))
 				.build();
 	}
-
 }
