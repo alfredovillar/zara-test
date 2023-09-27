cd52dec75d15d5db9d229a2dfab8f1bc7a778fd2
alfredo <vdevendetta77@gmail.com>
2023-09-27 09:11:00 +0200

    Improved code

--- src/main/java/com/inditex/zarachallenge/infrastructure/ProductApiRestRepository.java
+++ src/main/java/com/inditex/zarachallenge/infrastructure/ProductApiRestRepository.java
@@ -1,5 +1,7 @@
 package com.inditex.zarachallenge.infrastructure;
 
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
@@ -16,9 +18,12 @@ public class ProductApiRestRepository  {
     }
 
     public List<Integer> getSimilarIdsByProduct(Integer productId) {
+        ParameterizedTypeReference<List<Integer>> parameterizedTypeReference =
+                new ParameterizedTypeReference<>(){};
 
         final String uri = "http://localhost:3005/product/{productId}/similarids";
-        ResponseEntity<List> list = restTemplate.getForEntity(uri, List.class, productId);
-        return list.getBody();
+        String finalUri=uri.replace("{productId}",productId+"");
+        ResponseEntity<List<Integer>> response = restTemplate.exchange(finalUri, HttpMethod.GET,null ,parameterizedTypeReference);;
+        return response.getBody();
     }
 }
